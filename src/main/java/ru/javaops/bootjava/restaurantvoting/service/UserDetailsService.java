3b9ce5c0071fe90707bc8c3f796f8bc16c6d3130
gagarina6794 <gagarina6794@gmail.com>
2019-03-14 15:04:55 +0200

    1 10 security

--- src/main/java/ru/javaops/bootjava/restaurantvoting/service/UserDetailsService.java
+++ src/main/java/ru/javaops/bootjava/restaurantvoting/service/UserDetailsService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.javaops.bootjava.restaurantvoting.AuthUser;
 import ru.javaops.bootjava.restaurantvoting.model.User;
 import ru.javaops.bootjava.restaurantvoting.repository.UserRepository;
